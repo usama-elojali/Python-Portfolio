8cae7943c3381402e0a3cda51a92305e6f10e498
Usama Elojali <110250823+usama-elojali@users.noreply.github.com>
2025-06-25 17:10:43 +0100

    Update main.py

--- Tip Calculator/main.py
+++ Tip Calculator/main.py
@@ -5,4 +5,4 @@ people = int(input("How many people to split the bill?\n"))
 tip_percentage = tip / 100
 tip_amount = (bill / people) * (tip_percentage + 1)
 round_tip_amount = round(tip_amount, 2)
-print(f"Each person should pay: {round_tip_amount}")
+print(f"Each person should pay: ${round_tip_amount}")
